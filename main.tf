9a1b0c880d917acd261a3b3c220a03ba2b692b7c
joekendal <13680617+joekendal@users.noreply.github.com>
2021-05-05 04:58:54 +0100

    Removed nested provider

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-provider "aws" {
-  region = var.region
-}
-
 module "dynamodb" {
   source = "./modules/dynamodb"
   stage  = var.stage
